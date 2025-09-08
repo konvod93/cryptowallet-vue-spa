0fb6caa1f05907d9671bca9a05a4e5287d76c278
Konstantin <konvod93@gmail.com>
2025-09-08 18:46:19 +0300

    added updating of  currency

--- src/stores/walletStore.ts
+++ src/stores/walletStore.ts
@@ -3,28 +3,29 @@
 import { defineStore } from 'pinia';
 import type { Wallet } from '../types/wallet';
 
+
 export const useWalletStore = defineStore('wallet', {
   state: (): Wallet => ({
     balance: 1000.00,
     currency: 'TON',
-      }),
+  }),
   actions: {
     updateBalance(newBalance: number) {
       this.balance = newBalance;
-    },    
+    },
    
     sendFunds(_to: string, amount: number, _memo?: string) {
       if (amount > this.balance) throw new Error('Недостаточно средств');
 
-      this.balance -= amount;    
+      this.balance -= amount;
 
-      
+
     },
     receiveFunds(_from: string, amount: number, _memo?: string) {
       this.balance += amount;
-      
+
     },
-    
+
   },
   persist: true
 });
